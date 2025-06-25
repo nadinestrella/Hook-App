e9407f438fc1eb664431b3413394a0eac1df340f
nadinestrella <nadinestrella@gmail.com>
2025-06-25 11:35:01 +0200

    initial set up

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { HookApp } from './HookApp';
+
+import './index.css';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <HookApp />
+  </StrictMode>
+);
