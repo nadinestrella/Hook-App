9bb9aea2fd6794d6d0b7d9bf0df6084d6e32b64a
nadinestrella <nadinestrella@gmail.com>
2025-06-27 11:50:07 +0200

    custom hook

---
+++ src/hooks/useForm.ts
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+
+export const useForm = <T extends Record<string, any>>(initialForm: T) => {
+  const [formState, setFormState] = useState(initialForm);
+
+  const onInputChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = target;
+    setFormState({
+      ...formState,
+      // propiedades computadas de los objetos entre []
+      [name]: value,
+    });
+  };
+
+  return {
+    ...formState,
+    formState,
+    onInputChange,
+  };
+};
--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import './index.css';
-import { SimpleForm } from './02-useEffect/SimpleForm';
+
+import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* <HookApp /> */}
     {/* <CounterApp /> */}
     {/* <CounterWithCustomHook /> */}
-    <SimpleForm />
+    {/* <SimpleForm /> */}
+    <FormWithCustomHook />
   </StrictMode>
 );
